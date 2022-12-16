bde5ce1ce87df0cd14f6c3779cf96e4c217d9ad5
Lidier López <llopezraccioppe@danielcastelao.org>
2022-12-16 09:21:27 +0100

    fue mi primito

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Cpu"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Monitor"},{"p":"<Unnamed>","l":"Ordenador"},{"p":"<Unnamed>","l":"Teclado"}];updateSearchResults();
